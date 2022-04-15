9e4b4ae81c144288e665119e7fad3c7cba6cdb42
luyao514 <1824056030@qq.com>
2022-04-15 10:19:28 +0800

    chore: modify the "printWidth" config of prettier

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author       : 卢瑶
+ * @Date         : 2022-03-17 16:25:52
+ * @LastEditTime : 2022-04-15 10:16:15
+ * @LastEditors  : 卢瑶
+ * @Description  :
+ * @FilePath     : /vite-vue3-ts-ly/src/main.ts
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
